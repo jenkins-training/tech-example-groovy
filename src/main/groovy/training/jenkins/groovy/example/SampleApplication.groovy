e350287e08539902dad4900519aee06f85ed01cd
Jason Taylor <jason@jasongtaylor.com>
2017-03-28 22:04:49 -0400

    first commit with Groovy code

---
+++ src/main/groovy/training/jenkins/groovy/example/SampleApplication.groovy
@@ -0,0 +1,12 @@
+package training.jenkins.groovy.example
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SampleApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run SampleApplication, args
+	}
+}
---
+++ src/test/groovy/training/jenkins/groovy/example/SampleApplicationTests.groovy
@@ -0,0 +1,16 @@
+package training.jenkins.groovy.example
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
